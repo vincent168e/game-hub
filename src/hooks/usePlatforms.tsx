ee0f6659a373634054060009fb92a05210648eaf
Vincent Tsang <vincent168e@gmail.com>
2024-01-19 12:04:41 +0800

    Refactor: Remove duplicated platform interfaces

--- src/hooks/usePlatforms.tsx
+++ src/hooks/usePlatforms.tsx
@@ -4,7 +4,7 @@ import platforms from "../data/platforms";
 import apiClient from "../services/api-client";
 import { FetchResponse } from "../services/api-client";
 
-interface Platform {
+export interface Platform {
   id: number;
   name: string;
   slug: string;
