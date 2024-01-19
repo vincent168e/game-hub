5a317a766a20bee8f5e7479af8c0ec9d8fb88547
Vincent Tsang <vincent168e@gmail.com>
2024-01-19 18:03:52 +0800

    Fetch game details

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -12,6 +12,7 @@ import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
 import Emoji from "./Emoji";
+import { Link } from "react-router-dom";
 
 interface Props {
   game: Game;
@@ -20,7 +21,9 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card>
-      <Image src={getCroppedImageUrl(game.background_image)} />
+      <Link to={"/games/" + game.slug}>
+        <Image src={getCroppedImageUrl(game.background_image)} />
+      </Link>
       <CardBody>
         <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIconList
@@ -29,7 +32,7 @@ const GameCard = ({ game }: Props) => {
           <CriticScore score={game.metacritic} />
         </HStack>
         <Heading fontSize="2xl">
-          {game.name}
+          <Link to={"/games/" + game.slug}>{game.name}</Link>
           <Box paddingY={3}>
             <Emoji rating={game.rating_top} />
           </Box>
