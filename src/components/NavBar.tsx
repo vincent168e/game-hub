3d738dca6434bbf6fd7ac51bff9abcd22d0c92a5
Vincent Tsang <vincent168e@gmail.com>
2024-01-19 16:41:54 +0800

    Refactor: Manage state with Zustand

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,18 +3,14 @@ import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
-interface Props {
-  onSearch: (searchText: string) => void;
-}
-
-const NavBar = ({ onSearch }: Props) => {
+const NavBar = () => {
   return (
     <HStack padding="10px">
       <Image src={logo} boxSize="50px" />
       <Heading fontSize="xl" whiteSpace="nowrap">
         Game Hub
       </Heading>
-      <SearchInput onSearch={onSearch} />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
